76f8d0b14c67f58fa442ab7ee6cf67c1721f7884
Lucia Perrini <lucia.perrini@gmail.com>
2012-10-11 16:01:44 +0000

    other histos

--- src/Analysis.cxx
+++ src/Analysis.cxx
@@ -209,7 +209,8 @@ void Analysis::BeginInputData( const SInputData& ) throw( SError ) {
 	// Lumi weights
 
 	if(is2012_52) LumiWeights_ = new reweight::LumiReWeighting("Summer12_PU.root", "dataPileUpHistogram_True_2012.root","mcPU","pileup");
-	else if (is2011) LumiWeights_ = new reweight::LumiReWeighting("Fall11_PU.root", "dataPileUpHistogram_True_2011.root","mcPU","pileup");
+	else if (useTruePileUp && is2011) LumiWeights_ = new reweight::LumiReWeighting("Fall11_PU.root", "dataPileUpHistogram_True_2011.root","mcPU","pileup");
+	else if (!useTruePileUp && is2011) LumiWeights_ = new reweight::LumiReWeighting("Fall11_PU_observed.root", "dataPileUpHistogram_Observed_2011.root","mcPU","pileup");
 	else LumiWeights_ = new reweight::LumiReWeighting("Summer12_PU_53X.root", "dataPileUpHistogram_True_2012.root","mcPU","pileup");
 
 	return;
@@ -349,7 +350,7 @@ void Analysis::ExecuteEvent( const SInputData&, Double_t ) throw( SError ) {
 	m_logger << DEBUG << " Now executing event " << m->eventNumber << " in a run " << m->runNumber << SLogger::endmsg;
 	double PUWeight = 1.0;
 	double nPU = 0.0;
-	if(useTruePileUp && is2011)  nPU = m->PUInfo_true;
+	if(useTruePileUp || !is2011)  nPU = m->PUInfo_true;
 	else nPU = m->PUInfo_Bunch0;
 	if(isSimulation){	
 		PUWeight = LumiWeights_->weight( nPU );
@@ -360,8 +361,7 @@ void Analysis::ExecuteEvent( const SInputData&, Double_t ) throw( SError ) {
 	if(!useTruePileUp && is2011){ 
 		Hist("h_nPU_raw")->Fill(m->PUInfo_Bunch0);
 		Hist("h_nPU_reweight")->Fill(m->PUInfo_Bunch0,PUWeight);
-	}else
-	if(useTruePileUp && is2011){ 
+	}else{ 
 		Hist("h_nPU_raw")->Fill(m->PUInfo_true);
 		Hist("h_nPU_reweight")->Fill(m->PUInfo_true,PUWeight);
 	}
