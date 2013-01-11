88c45241aaa4aaace1d070e6830861dcca263082
Pavel Jez <pavel.jez@cern.ch>
2013-01-11 22:53:08 +0000

    handling scripts

---
+++ macros/20130111_FILTER_D_Ele_SubmitAll.sh
@@ -0,0 +1,472 @@
+cd 20130111_FILTER_D_Ele/job1
+echo "Submitting job no. 1... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_1.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job2
+echo "Submitting job no. 2... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_2.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job3
+echo "Submitting job no. 3... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_3.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job4
+echo "Submitting job no. 4... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_4.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job5
+echo "Submitting job no. 5... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_5.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job6
+echo "Submitting job no. 6... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_6.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job7
+echo "Submitting job no. 7... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_7.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job8
+echo "Submitting job no. 8... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_8.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job9
+echo "Submitting job no. 9... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_9.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job10
+echo "Submitting job no. 10... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_10.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job11
+echo "Submitting job no. 11... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_11.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job12
+echo "Submitting job no. 12... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_12.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job13
+echo "Submitting job no. 13... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_13.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job14
+echo "Submitting job no. 14... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_14.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job15
+echo "Submitting job no. 15... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_15.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job16
+echo "Submitting job no. 16... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_16.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job17
+echo "Submitting job no. 17... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_17.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job18
+echo "Submitting job no. 18... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_18.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job19
+echo "Submitting job no. 19... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_19.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job20
+echo "Submitting job no. 20... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_20.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job21
+echo "Submitting job no. 21... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_21.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job22
+echo "Submitting job no. 22... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_22.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job23
+echo "Submitting job no. 23... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_23.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job24
+echo "Submitting job no. 24... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_24.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job25
+echo "Submitting job no. 25... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_25.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job26
+echo "Submitting job no. 26... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_26.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job27
+echo "Submitting job no. 27... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_27.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job28
+echo "Submitting job no. 28... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_28.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job29
+echo "Submitting job no. 29... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_29.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job30
+echo "Submitting job no. 30... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_30.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job31
+echo "Submitting job no. 31... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_31.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job32
+echo "Submitting job no. 32... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_32.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job33
+echo "Submitting job no. 33... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_33.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job34
+echo "Submitting job no. 34... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_34.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job35
+echo "Submitting job no. 35... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_35.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job36
+echo "Submitting job no. 36... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_36.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job37
+echo "Submitting job no. 37... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_37.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job38
+echo "Submitting job no. 38... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_38.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job39
+echo "Submitting job no. 39... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_39.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job40
+echo "Submitting job no. 40... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_40.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job41
+echo "Submitting job no. 41... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_41.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job42
+echo "Submitting job no. 42... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_42.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job43
+echo "Submitting job no. 43... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_43.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job44
+echo "Submitting job no. 44... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_44.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job45
+echo "Submitting job no. 45... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_45.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job46
+echo "Submitting job no. 46... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_46.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job47
+echo "Submitting job no. 47... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_47.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job48
+echo "Submitting job no. 48... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_48.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job49
+echo "Submitting job no. 49... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_49.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job50
+echo "Submitting job no. 50... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_50.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job51
+echo "Submitting job no. 51... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_51.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job52
+echo "Submitting job no. 52... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_52.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job53
+echo "Submitting job no. 53... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_53.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job54
+echo "Submitting job no. 54... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_54.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job55
+echo "Submitting job no. 55... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_55.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job56
+echo "Submitting job no. 56... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_56.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job57
+echo "Submitting job no. 57... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_57.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job58
+echo "Submitting job no. 58... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_58.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job59
+echo "Submitting job no. 59... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_59.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job60
+echo "Submitting job no. 60... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_60.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job61
+echo "Submitting job no. 61... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_61.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job62
+echo "Submitting job no. 62... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_62.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job63
+echo "Submitting job no. 63... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_63.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job64
+echo "Submitting job no. 64... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_64.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job65
+echo "Submitting job no. 65... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_65.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job66
+echo "Submitting job no. 66... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_66.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job67
+echo "Submitting job no. 67... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_67.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job68
+echo "Submitting job no. 68... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_68.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job69
+echo "Submitting job no. 69... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_69.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job70
+echo "Submitting job no. 70... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_70.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job71
+echo "Submitting job no. 71... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_71.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job72
+echo "Submitting job no. 72... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_72.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job73
+echo "Submitting job no. 73... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_73.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job74
+echo "Submitting job no. 74... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_74.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job75
+echo "Submitting job no. 75... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_75.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job76
+echo "Submitting job no. 76... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_76.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job77
+echo "Submitting job no. 77... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_77.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job78
+echo "Submitting job no. 78... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_78.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job79
+echo "Submitting job no. 79... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_79.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job80
+echo "Submitting job no. 80... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_80.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job81
+echo "Submitting job no. 81... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_81.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job82
+echo "Submitting job no. 82... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_82.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job83
+echo "Submitting job no. 83... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_83.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job84
+echo "Submitting job no. 84... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_84.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job85
+echo "Submitting job no. 85... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_85.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job86
+echo "Submitting job no. 86... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_86.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job87
+echo "Submitting job no. 87... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_87.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job88
+echo "Submitting job no. 88... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_88.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job89
+echo "Submitting job no. 89... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_89.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job90
+echo "Submitting job no. 90... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_90.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job91
+echo "Submitting job no. 91... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_91.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job92
+echo "Submitting job no. 92... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_92.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job93
+echo "Submitting job no. 93... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_93.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job94
+echo "Submitting job no. 94... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_94.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job95
+echo "Submitting job no. 95... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_95.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job96
+echo "Submitting job no. 96... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_96.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job97
+echo "Submitting job no. 97... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_97.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job98
+echo "Submitting job no. 98... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_98.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job99
+echo "Submitting job no. 99... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_99.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job100
+echo "Submitting job no. 100... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_100.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job101
+echo "Submitting job no. 101... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_101.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job102
+echo "Submitting job no. 102... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_102.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job103
+echo "Submitting job no. 103... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_103.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job104
+echo "Submitting job no. 104... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_104.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job105
+echo "Submitting job no. 105... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_105.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job106
+echo "Submitting job no. 106... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_106.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job107
+echo "Submitting job no. 107... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_107.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job108
+echo "Submitting job no. 108... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_108.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job109
+echo "Submitting job no. 109... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_109.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job110
+echo "Submitting job no. 110... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_110.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job111
+echo "Submitting job no. 111... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_111.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job112
+echo "Submitting job no. 112... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_112.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job113
+echo "Submitting job no. 113... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_113.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job114
+echo "Submitting job no. 114... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_114.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job115
+echo "Submitting job no. 115... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_115.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job116
+echo "Submitting job no. 116... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_116.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job117
+echo "Submitting job no. 117... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_117.sh
+cd ../..
+cd 20130111_FILTER_D_Ele/job118
+echo "Submitting job no. 118... " 
+qsub -q localgrid@cream02 -o script.stdout -e script.stderr 20130111_FILTER_D_Ele_118.sh
+cd ../..
