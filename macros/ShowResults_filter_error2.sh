44c2a85f1a353e992c99cd48867a2dd44650b2e6
Pavel Jez <pavel.jez@cern.ch>
2013-01-14 00:17:28 +0000

    small fixes

--- macros/ShowResults_filter_error2.sh
+++ macros/ShowResults_filter_error2.sh
@@ -8,9 +8,9 @@ for block in  `seq -s ' ' 1 ${max}`; do
     more ${name}/job${block}/*stdout* | grep -i '(0)'
     more ${name}/job${block}/*stdout* | grep -i 'ERROR'
     more ${name}/job${block}/*stdout* | grep -i 'FATAL'
-    more ${name}_job${block}/*stderr* | grep -i 'error'
-    more ${name}_job${block}/*stderr* | grep -i 'segmentation'
-    more ${name}_job${block}/*stderr* | grep -i 'fatal'
+    more ${name}/job${block}/*stderr* | grep -i 'error'
+    more ${name}/job${block}/*stderr* | grep -i 'segmentation'
+    more ${name}/job${block}/*stderr* | grep -i 'fatal'
     ls -ltrh ${name}/job${block} | grep root | grep Filter
     #cd ${name}_job${block}
     more ${name}/job${block}/total.txt
